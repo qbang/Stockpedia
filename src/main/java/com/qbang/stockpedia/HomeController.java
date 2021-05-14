e0e7b520b70ef4457d12f89a1154f93db27485c7
qbang <vhfhddl0101@naver.com>
2021-05-14 16:04:04 +0900

    fix: /에 리턴해주는 페이지 detail.jsp로 변경

--- src/main/java/com/qbang/stockpedia/HomeController.java
+++ src/main/java/com/qbang/stockpedia/HomeController.java
@@ -53,7 +53,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model){
-		return "home";
+		return "detail";
 	}
 	
 	@RequestMapping(value = "/detail", method = RequestMethod.GET)
