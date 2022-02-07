d51f8f1c4f9662525f643710fa859237b62bf814
qbang <vhfhddl0101@naver.com>
2022-02-07 16:22:10 +0900

    fix: 요청 api 파라미터 추가

--- src/test/java/RequestStockTest.java
+++ src/test/java/RequestStockTest.java
@@ -2,12 +2,14 @@ import com.qbang.stockpedia.impl.RequestStockService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
+
 public class RequestStockTest {
     @Autowired
     RequestStockService requestStockService = new RequestStockService();
 
     @Test
-    public void testConnect() {
-        requestStockService.requestItemInfo();
+    public void testConnect() throws IOException {
+        requestStockService.requestItemInfo(requestStockService.getItemCode());
     }
 }
